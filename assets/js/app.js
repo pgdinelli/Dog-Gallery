1b72179fa7f9e29f0c7997dd89b672ef4d549fe2
dinellipaulo <pgsdinelli@gmail.com>
2025-12-26 22:38:06 -0400

    implementado tratamento de exceções

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,28 @@
-(function() {
+(function () {
     const image = document.querySelector('#image');
     const imgBtn = document.querySelector('#imgBtn');
     const breed = document.querySelector('#breed');
 
-    async function getImg(){
-        const url = 'https://dog.ceo/api/breeds/image/random';
-        const response = await fetch(url);
-        const data = await response.json();
-        return data.message;
+    async function getImg() {
+        try {
+            const url = 'https://dog.ceo/api/breeds/image/random';
+            const response = await fetch(url);
+            const data = await response.json();
+            return data.message;
+        } catch (e) {
+            breed.textContent = `Erro ao pegar imagem`;
+            return;
+        }
     }
 
-    imgBtn.addEventListener('click', async () => { 
-        const data = await getImg();
-        image.src = data;
-        breed.textContent = data.split('/')[4];
+    imgBtn.addEventListener('click', async () => {
+        try {
+            const data = await getImg();
+            image.src = data;
+            breed.textContent = data.split('/')[4];
+        } catch (error) {
+            return;
+        }
     });
+
 })();
