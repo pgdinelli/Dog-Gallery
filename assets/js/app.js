aacd8a4b947e9cd29e4dbeec376e69c5cd502460
dinellipaulo <pgsdinelli@gmail.com>
2025-12-26 21:37:24 -0400

    adicionado botão de trocar imagem

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+(function() {
+    const imageContainer = document.querySelector('.image-container');
+    const image = document.querySelector('#image');
+    const imgBtn = document.querySelector('#imgBtn');
+
+    imgBtn.addEventListener('click', () => {
+        alert('botão clicado')
+    });
+})();
